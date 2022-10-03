4359a022415c8c77e66d60b451c083afdf2ec19c
Emmanuel <emmanuel.dacquah.gmail.com>
2022-10-03 14:03:28 +0300

    created the first model and super user with name: admin

--- To_do_list/To_Do/models.py
+++ To_do_list/To_Do/models.py
@@ -1,3 +1,9 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_to_do = models.CharField(max_length=200)
+    complete = models.BooleanField(default="False")
+    added_time = models.DateTimeField(datetime.now())
+    
--- To_do_list/To_Do/views.py
+++ To_do_list/To_Do/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Task
 # Create your views here.
 def index(request):
+    task_obj = Task.objects.all()
+    print(task_obj.task_to_do)
     return render(request, 'To_Do/index.html')
