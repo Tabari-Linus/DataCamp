d00d5b17365711a3733a5654db6cde594417fe18
Mr Lii <leetamparega@gmail.com>
2025-01-05 12:26:15 +0000

    data folder and data

--- To_do_list/To_Do/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "To_Do"
-urlpatterns = [
-    path('',views.index, name="index"),
-    path('To_Do/add_task/', views.add_task, name="add_task"),
-
-]
